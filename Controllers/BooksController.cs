383206cafc4c9b42fe1d5947b3d53b24b8a66243
emiliano-ma <emilianomainero@gmail.com>
2020-12-03 21:27:53 +0100

    Adds Http PATCH action to handle books checkout/checkin. Updates modifiedDate with DateTime.Now

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -10,101 +10,130 @@ using LibraryApi.Models;
 
 namespace LibraryApi.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BooksController : ControllerBase
+  [Route("api/[controller]")]
+  [ApiController]
+  public class BooksController : ControllerBase
+  {
+    private readonly LibraryContext _context;
+
+    public BooksController(LibraryContext context)
     {
-        private readonly LibraryContext _context;
-
-        public BooksController(LibraryContext context)
-        {
-            _context = context;
-        }
-
-        // GET: api/Books
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
-        {
-            return await _context.Books.ToListAsync();
-        }
-
-        // GET: api/Books/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Book>> GetBook(int id)
-        {
-            var book = await _context.Books.FindAsync(id);
-
-            if (book == null)
-            {
-                return NotFound();
-            }
-
-            return book;
-        }
-
-        // PUT: api/Books/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutBook(int id, Book book)
-        {
-            if (id != book.BookId)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(book).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!BookExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Books
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
-        [HttpPost]
-        public async Task<ActionResult<Book>> PostBook(Book book)
-        {
-            _context.Books.Add(book);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction(nameof(GetBook), new { id = book.BookId }, book);
-        }
-
-        // DELETE: api/Books/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<Book>> DeleteBook(int id)
-        {
-            var book = await _context.Books.FindAsync(id);
-            if (book == null)
-            {
-                return NotFound();
-            }
-
-            _context.Books.Remove(book);
-            await _context.SaveChangesAsync();
-
-            return book;
-        }
-
-        private bool BookExists(int id)
-        {
-            return _context.Books.Any(e => e.BookId == id);
-        }
+      _context = context;
     }
+
+    // GET: api/Books
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
+    {
+      return await _context.Books.ToListAsync();
+    }
+
+    // GET: api/Books/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Book>> GetBook(int id)
+    {
+      var book = await _context.Books.FindAsync(id);
+
+      if (book == null)
+      {
+        return NotFound();
+      }
+
+      return book;
+    }
+
+
+    // PATCH: api/Books/5
+    [HttpPatch("{id}")]
+    public async Task<ActionResult<Book>> PatchBook(int id, Book book)
+    {
+      var bookPatch = await _context.Books.FindAsync(id);
+
+      if (bookPatch == null)
+      {
+        return NotFound();
+      }
+
+      bookPatch.ReaderId = book.ReaderId;
+      bookPatch.Available = book.Available;
+      bookPatch.ModifiedDate = DateTime.Now;
+
+      try
+      {
+        await _context.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        throw;
+      }
+
+      return bookPatch;
+    }
+
+
+    // PUT: api/Books/5
+    // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+    // more details see https://aka.ms/RazorPagesCRUD.
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutBook(int id, Book book)
+    {
+      if (id != book.BookId)
+      {
+        return BadRequest();
+      }
+
+      _context.Entry(book).State = EntityState.Modified;
+
+      try
+      {
+        await _context.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!BookExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+    // POST: api/Books
+    // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+    // more details see https://aka.ms/RazorPagesCRUD.
+    [HttpPost]
+    public async Task<ActionResult<Book>> PostBook(Book book)
+    {
+      _context.Books.Add(book);
+      await _context.SaveChangesAsync();
+
+      return CreatedAtAction(nameof(GetBook), new { id = book.BookId }, book);
+    }
+
+    // DELETE: api/Books/5
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<Book>> DeleteBook(int id)
+    {
+      var book = await _context.Books.FindAsync(id);
+      if (book == null)
+      {
+        return NotFound();
+      }
+
+      _context.Books.Remove(book);
+      await _context.SaveChangesAsync();
+
+      return book;
+    }
+
+    private bool BookExists(int id)
+    {
+      return _context.Books.Any(e => e.BookId == id);
+    }
+  }
 }
