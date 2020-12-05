63d0c1da67134e8044b55aec94fbca26068584bd
emiliano-ma <emilianomainero@gmail.com>
2020-12-05 21:26:41 +0100

    Adds folder structure and sets up testing evironment with xUnit test library

---
+++ test/LibraryApiTests/BooksControllerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+using LibraryApi.Controllers;
+using LibraryApi.Data;
+
+
+
+namespace LibraryApi.Tests.UnitTests
+{
+  public class BooksControllerTests
+  {
+    
+  }
+}
